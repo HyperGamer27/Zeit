3f5f09e479dc8f1c4b2c2b8b51dab79522eeb5a5
HyperGamer27 <hypergamer2712@gmail.com>
2017-10-07 21:31:31 +0300

    Update Zeit.py

--- Zeit.py
+++ Zeit.py
@@ -1,1 +1,9 @@
-
+import os
+import sys
+class Zeit(object):
+    """docstring for Zeit."""
+    def __init__(self, arg):
+        super(Zeit, self).__init__()
+        self.arg = arg
+        print(self.arg)
+zeit = Zeit("Hello World!")
